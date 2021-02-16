2c3e484ff7a37170ce01b495bacb0c243f13637f
Martynas <nbtsst@gmail.com>
2021-02-16 23:18:55 +0200

    Products object added to return of index page

--- src/main/java/com/learn/thymeleaf/catalog/controllers/IndexController.java
+++ src/main/java/com/learn/thymeleaf/catalog/controllers/IndexController.java
@@ -1,13 +1,23 @@
 package com.learn.thymeleaf.catalog.controllers;
 
+import com.learn.thymeleaf.catalog.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    private final ProductService productService;
+
+    public IndexController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping({"", "/", "/index", "/home"})
-    public String getHomepage() {
+    public String getHomepage(Model model) {
+
+        model.addAttribute("products", productService.listProducts());
 
         return "index";
     }

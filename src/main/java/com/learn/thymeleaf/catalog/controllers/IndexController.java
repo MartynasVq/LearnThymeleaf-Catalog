ca788caca3b6d3394b7b142335a846b1e85515d7
Martynas <nbtsst@gmail.com>
2021-02-17 22:31:53 +0200

    Added spring security config and dependencies

--- src/main/java/com/learn/thymeleaf/catalog/controllers/IndexController.java
+++ src/main/java/com/learn/thymeleaf/catalog/controllers/IndexController.java
@@ -4,6 +4,7 @@ import com.learn.thymeleaf.catalog.services.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
@@ -20,5 +21,11 @@ public class IndexController {
         model.addAttribute("products", productService.listProducts());
 
         return "index";
+    }
+
+    @RequestMapping("logout-success")
+    public String youreLoggedOut(){
+
+        return "logout-success";
     }
 }
